8776eeb716bcbd55d77129150bb02eddcabdbaef
smk627751 <smk627751@gmail.com>
2023-02-16 12:13:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import io from 'socket.io-client'
 import Chat from './Chat'
+import Login from './login'
 
 // const endpoint = "http://localhost:5000" // if we run th server in local host
 const endpoint = "https://chat-app-server-uagt.onrender.com"
@@ -11,24 +12,12 @@ function App() {
   const [user,setUser] = useState('')
   const [room,setRoom] = useState('')
   const [chat,setChat] = useState(false)
-  const login = (e) => {
-    e.preventDefault()
-    setChat((prev)=> {return !prev})
-    socket.emit("new-user",user,room)
-  }
-
+ 
   return (
     <>
       {chat ?
          <Chat socket={socket} user={user} room={room}/>
-        :<>
-          <form className='login' onSubmit={login}>
-            <h3>Join Room</h3>
-            <input type='text' className='textBox' placeholder='Enter your name' required onInput={(e)=>setUser(e.target.value)}/>
-            <input type='text' className='textBox' placeholder='Enter Room id' required onInput={(e)=>setRoom(e.target.value)}/>
-            <input type="submit" value="Start Chat" className='submitBtn'/>
-          </form>
-        </>}
+        :<Login socket ={socket} setUser={setUser} setRoom={setRoom} setChat={setChat} user={user} room={room}/>}
         <section></section>  
     </>
   );
